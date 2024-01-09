fdb773ea3d0f1a355af0e6b311796fe74e13896e
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2024-01-09 16:36:21 +0200

    fix: layout error in portrait mode (small screens)

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -55,7 +55,7 @@ const Services = () => {
         </div>
 
         {services.map((service, index) => (
-          <section key={index} className="mt-8 bg-background p-4">
+          <section key={index} className="bg-background p-4">
             <div className="mb-4 flex items-center">
               {service.icon}
               <h2 className="ml-4 text-xl font-bold">{service.title}</h2>
@@ -64,9 +64,9 @@ const Services = () => {
           </section>
         ))}
 
-        <div className="col-span-2">
+        <div className="md:col-span-2">
           {/* Additional Text */}
-          <section className="mt-8">
+          <section>
             <p className="text-lg">
               Elevate your business to new heights with Hypercliq&apos;s
               innovative services. Our commitment to excellence and expertise in
@@ -78,7 +78,7 @@ const Services = () => {
         </div>
       </div>
 
-      <section className="mt-8 flex items-center justify-center px-4 md:px-8">
+      <section className="mt-8 flex items-center justify-center">
         <Link
           className="text-md rounded-md bg-accent px-3.5 py-2.5 font-semibold text-onAccent shadow-sm hover:brightness-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
           href="/contact"
