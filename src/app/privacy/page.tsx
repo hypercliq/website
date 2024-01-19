c99f745b8e09e3e58b5659ba56c89442aa714d7e
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2024-01-19 15:21:14 +0200

    fix: legal pages text revision (#20)

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
 import PageTemplate from '@/app/components/PageTemplate'
+import { EmailSVG } from '../components/EmailSVG'
 
 // create metadata for this page
 export const metadata: Metadata = {
@@ -20,13 +21,7 @@ const PrivacyPolicy = () => {
           </Link>
           {''}, and we do not collect any browsing information or statistics
           while you browse our website.
-        </>
-      ),
-    },
-    {
-      title: 'Contacting Us',
-      content: (
-        <>
+          <br />
           The only form of data collection on our website is when you choose to
           contact us. If you decide to email us using the provided email
           address, we will receive your email address and any additional
@@ -51,6 +46,32 @@ const PrivacyPolicy = () => {
           You can choose not to provide any personal information when browsing
           our website. If you choose to contact us, you are consenting to the
           use of the provided information for the purpose of communication.
+        </>
+      ),
+    },
+    {
+      title: 'Right to Be Forgotten',
+      content: (
+        <>
+          You have the right to request the deletion of your personal
+          information from our records. If you wish to exercise this right,
+          please contact us at{' '}
+          <EmailSVG aria-hidden="true" email="info@hypercliq.com" />. Upon
+          receiving your request, we will promptly delete your personal
+          information from our database, and you will no longer be identifiable
+          through the data we previously collected.
+        </>
+      ),
+    },
+    {
+      title: 'Use of Local Storage',
+      content: (
+        <>
+          Our website uses local storage to enhance your experience by
+          remembering your theme preference. This data is stored on your own
+          browser and can be cleared at any time in your browser settings. We do
+          not use local storage to collect personal information or share it with
+          third parties.
         </>
       ),
     },
